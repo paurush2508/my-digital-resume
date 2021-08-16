703f7f658944d84080582f8ccf2c555c71d23b95
paurush2508 <psrivastava@dealersocket.com>
2021-08-16 20:58:16 +0530

    add indian flag project to portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -6,13 +6,9 @@ const Portfolio = ({ data }) => {
       var projectImage = "images/portfolio/" + projects.image;
       return (
         <div key={projects.title} className="columns portfolio-item">
-          <div className="item-wrap">
+          <div className="item-wrap" style={{ boxShadow: "2px 2px" }}>
             <a href={projects.url} title={projects.title}>
-              <img
-                alt={projects.title}
-                src={projectImage}
-                style={{ border: "1px solid black", padding: "4px" }}
-              />
+              <img alt={projects.title} src={projectImage} />
 
               <div className="overlay">
                 <div className="portfolio-item-meta">
@@ -25,7 +21,9 @@ const Portfolio = ({ data }) => {
               </div>
             </a>
           </div>
-          <p style={{textAlign: "center", color: 'black'}}>{projects.title}</p>
+          <p style={{ textAlign: "center", color: "black" }}>
+            {projects.title}
+          </p>
         </div>
       );
     });
@@ -37,12 +35,7 @@ const Portfolio = ({ data }) => {
         <div className="twelve columns collapsed">
           <h1 style={{color: 'black'}}>Check Out Some of My Works.</h1>
           <div
-            style={{
-              width: "100%",
-              display: "grid",
-              gridTemplateColumns: "auto auto auto auto",
-              gridGap: "10px",
-            }}
+            className="project-item"           
           >
             {projects}
           </div>
